3f085d15535aee974699b24b36ccb04cec3fe1bc
Jonathan Bergqvist <jonathan@seednode.io>
2022-10-01 21:12:26 +0000

    Configure and instantiate datamodule using Hydra instead

--- train.py
+++ train.py
@@ -3,12 +3,9 @@ from hydra.utils import instantiate
 from pytorch_lightning import Trainer
 from omegaconf import DictConfig
 
-from src.data.modules.image import ImageDataModule
-
-
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def main(cfg: DictConfig):
-    datamodule = ImageDataModule(**cfg.datamodule)
+    datamodule = instantiate(cfg.datamodule)
     model = instantiate(cfg.model)
     trainer = Trainer()
 
