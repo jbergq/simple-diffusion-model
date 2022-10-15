61e111816d49d1ec7ffca5169b407c7af0378e35
Jonathan Bergqvist <jonathan@seednode.io>
2022-10-15 12:30:56 +0200

    Add visualization plot of positional encoding vectors to script. Add plot as image in README

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Simple Diffusion model
 
 Implementation of denoising diffusion model described in [Denoising Diffusion Probabilistic Models](https://arxiv.org/abs/2006.11239).
+
+## Implementation
+
+### Positional encoding
+
+The transformer sinusoidal positional encoding is used to encode the time step of each sample.
+
+```
+class PositionalEncoding(nn.Module):
+    def __init__(self, max_time_steps, embedding_size, n=10000):
+        super().__init__()
+
+        i = torch.arange(embedding_size // 2)
+        k = torch.arange(max_time_steps).unsqueeze(dim=1)
+
+        self.pos_embeddings = torch.zeros(
+            max_time_steps, embedding_size, requires_grad=False
+        )
+        self.pos_embeddings[:, 0::2] = torch.sin(k / (n ** (2 * i / embedding_size)))
+        self.pos_embeddings[:, 1::2] = torch.cos(k / (n ** (2 * i / embedding_size)))
+
+    def forward(self, t):
+        return self.pos_embeddings[t, :]
+```
+
+The figure below shows the elements of each positional encoding vector plotted along the x-axis for each time step on the y-axis.
+![positional encoding](./imgs/pos_enc.png)
--- scripts/positional_encoding_example.py
+++ scripts/positional_encoding_example.py
@@ -7,26 +7,55 @@ sys.path.append(str(Path.cwd().parent.absolute()))
 
 import torch
 import matplotlib.pyplot as plt
+from matplotlib import colors
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from src.model.network.layers.positional_encoding import PositionalEncoding
 
-x = torch.arange(100)
-print(x)
+# Get time steps
+num_t_steps = 100
+ts = torch.arange(num_t_steps)
+print(ts)
 
-# %%
+# %% Create positional encoding vector for each time step
 
-pos_enc = PositionalEncoding(1000, 28 ** 2)
+emb_size = 512
+pos_enc = PositionalEncoding(1000, emb_size)
 
-x_ = pos_enc(x)
+emb = pos_enc(ts)
 
-print(x_)
+print(emb)
 
-# %%
+# %% Plot sine and cosines over time
 
 num_plots = 10
-
 fig, axs = plt.subplots(num_plots, 1)
 
 for i in range(num_plots):
-    axs[i].plot(x_[1::2, 50+i])
+    axs[i].plot(emb[1::2, 50 + i])
+
+# %% Draw positional encoding vectors along x-axis
+
+# Index all elements and create img coords
+i = torch.arange(emb.shape[0] * emb.shape[1])
+xs = torch.div(i, emb.shape[1], rounding_mode="trunc")
+ys = torch.remainder(i, emb.shape[1])
+
+# Draw in image according to magnitude
+arr = torch.zeros((num_t_steps, emb_size))
+arr[xs, ys] = emb.flatten()
+
+# Show image with colorbar
+plt.figure(figsize=(10, 10))
+ax = plt.gca()
+im = plt.imshow(arr, cmap="gnuplot")
+
+plt.xlabel("Positional encoding vector")
+plt.ylabel("Time step")
+
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.05)
+plt.colorbar(im, cax=cax)
+plt.show()
+
 # %%
