a889e30d2ba212c432b7a46025a45afc839771c8
Jonathan Bergqvist <jonathan@seednode.io>
2022-10-20 21:31:04 +0200

    ResNetBlock updates

--- src/model/network/layers/resnet_block.py
+++ src/model/network/layers/resnet_block.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import torch
 import torch.nn as nn
 from torch import Tensor
 from einops import rearrange
@@ -33,7 +34,7 @@ class ResNetBlock(nn.Module):
         self,
         inplanes: int,
         planes: int,
-        t_emb_dim: Optional[int] = None,
+        t_dim: Optional[int] = None,
         activation: nn.Module = nn.SiLU,
         downsample: Optional[bool] = False,
         upsample: Optional[bool] = False,
@@ -43,9 +44,9 @@ class ResNetBlock(nn.Module):
         self.act = activation(inplace=True)
         conv1_stride = 2 if downsample else 1
 
-        self.t_emb_proj = (
-            nn.Sequential(self.act, nn.Linear(t_emb_dim, planes))
-            if t_emb_dim is not None
+        self.t_proj = (
+            nn.Sequential(self.act, nn.Linear(t_dim, planes))
+            if t_dim is not None
             else None
         )
         self.conv1 = conv3x3(inplanes, planes, conv1_stride)
@@ -54,43 +55,49 @@ class ResNetBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
 
         if downsample:
-            self.ds = nn.Sequential(
+            self.ds_skip = nn.Sequential(
                 conv1x1(inplanes, planes, conv1_stride), nn.BatchNorm2d(planes)
             )
         if upsample:
-            self.us_inp = nn.Sequential(
+            self.us_skip = nn.Sequential(
                 nn.Upsample(scale_factor=2), conv1x1(inplanes, planes)
             )
             self.us_hidden = nn.Upsample(scale_factor=2)
         self.downsample = downsample
         self.upsample = upsample
 
-    def forward(self, x: Tensor, t_emb: Tensor) -> Tensor:
-        hidden = x
+    def forward(self, x: Tensor, x_enc: Tensor = None, t_emb: Tensor = None) -> Tensor:
+        x_skip = x
 
-        # Down/up-sample.
+        # TODO(jonathanb): Move down/upsample out of block?
+
+        # Down/upsample.
         if self.downsample:
-            x = self.ds(x)
+            x_skip = self.ds_skip(x_skip)
         if self.upsample:
-            hidden = self.us_hidden(hidden)
-            x = self.us_inp(x)
+            x_skip = self.us_skip(x_skip)
+            x = self.us_hidden(x)
+
+        # Concatenate with encoder features.
+        if x_enc is not None:
+            x = torch.cat([x, x_enc], dim=1)
 
         # First hidden layer.
-        hidden = self.conv1(hidden)
-        hidden = self.bn1(hidden)
-        hidden = self.act(hidden)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.act(x)
 
         # Inject positional encoding in hidden state.
         if t_emb is not None:
-            t_emb = self.t_emb_proj(t_emb)
-            hidden = rearrange(t_emb, "b c -> b c 1 1") + hidden
+            t_emb = self.t_proj(t_emb)
+            x = rearrange(t_emb, "b c -> b c 1 1") + x
 
         # Second hidden layer.
-        hidden = self.conv2(hidden)
-        hidden = self.bn2(hidden)
+        x = self.conv2(x)
+        x = self.bn2(x)
 
         # Residual connection + activation.
-        hidden += x
-        hidden = self.act(hidden)
+        x += x_skip
+        out = self.act(x)
 
-        return hidden
+        return out
