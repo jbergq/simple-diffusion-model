f7446a0ef306e98fb00f4f839a1a10f8f867d71b
Jonathan Bergqvist <jonathan@seednode.io>
2022-11-11 20:38:02 +0100

    Rename arg in ConvBlock

--- src/model/network/layers/conv_block.py
+++ src/model/network/layers/conv_block.py
@@ -5,19 +5,19 @@ from torch import Tensor
 
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_size: int, out_size: int, act: Callable = nn.ReLU) -> None:
+    def __init__(self, in_size: int, out_size: int, activation: Callable = nn.ReLU) -> None:
         """Simple convolutional block: Conv2D -> BatchNorm -> Activation.
 
         Args:
             in_size (int): Size of input feature map.
             out_size (int): Size of output feature map.
-            act (Callable, optional): Activation function. Defaults to nn.ReLU.
+            activation (Callable, optional): Activation function. Defaults to nn.ReLU.
         """
         super().__init__()
 
         self.conv = nn.Conv2d(in_size, out_size, kernel_size=3, padding=1)
         self.bn = nn.BatchNorm2d(out_size)
-        self.act = act(inplace=True)
+        self.act = activation(inplace=True)
 
     def forward(self, x: Tensor) -> Tensor:
         x = self.conv(x)
