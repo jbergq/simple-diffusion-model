540faa0c6efddae81844c3a10af6f55fcb8f7547
Jonathan Bergqvist <jonathan@seednode.io>
2022-12-08 21:54:33 +0100

    Add beta scheduler

--- src/model/modules/diffusion_module.py
+++ src/model/modules/diffusion_module.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Dict, List, Tuple, Union
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import torch
 import torch.nn as nn
@@ -10,6 +10,7 @@ from torch.optim import Adam, Optimizer
 from torchmetrics.image.fid import FrechetInceptionDistance
 
 from src.utils.conversion import to_image
+from src.model.beta_scheduler import BetaScheduler
 
 
 class DiffusionModule(LightningModule):
@@ -19,19 +20,32 @@ class DiffusionModule(LightningModule):
         loss: Callable,
         t_min: int = 1,
         t_max: int = 100,
-        beta: float = 0.5,
+        beta_scheduler: Optional[Callable] = None,
     ) -> None:
         super().__init__()
 
         self.network = network
         self.loss = loss
+        self.beta_scheduler = beta_scheduler if beta_scheduler is not None else BetaScheduler("linear")
         self.fid = FrechetInceptionDistance()
 
-        self.t_min, self.t_max, self.beta = t_min, t_max, beta
+        self.t_min, self.t_max = t_min, t_max
+
+        # Get betas from scheduler. Pre-compute alphas and their cumulative product.
+        self.beta = self.beta_scheduler(t_max - t_min + 1)
+        self.alpha = 1 - self.beta
+        self.alpha_hat = torch.cumprod(self.alpha, dim=0)
+
         self.save_hyperparameters(ignore=["network", "loss"])
 
     def _is_last_batch(self, batch_idx: int):
         return batch_idx == self.trainer.num_training_batches - 1
+
+    def _beta_schedule(self, type: str = "linear"):
+        if type == "linear":
+            return
+        else:
+            raise ValueError("Only linear beta scheduling supported.")
 
     def training_step(self, batch: Dict, batch_idx: int, *args: Any, **kwargs: Any) -> Dict[str, Any]:
         imgs_real = batch["img"]
@@ -39,12 +53,13 @@ class DiffusionModule(LightningModule):
 
         # Sample random number of iterations to apply noise for in forward diffusion.
         ts = torch.randint(self.t_min, self.t_max, (num_imgs,), device=self.device)
-        alpha_hat = (1 - self.beta) ** ts
-        alpha_hat = alpha_hat[:, None, None, None]
+
+        alpha_hat_t = self.alpha_hat[ts]
+        alpha_hat_t = alpha_hat_t[:, None, None, None]
 
         # Apply noise to images (N steps forward diffusion in closed form).
         noise = torch.normal(0, 1, imgs_real.shape, device=self.device)
-        imgs_noisy = torch.sqrt(alpha_hat) * imgs_real + torch.sqrt(1 - alpha_hat) * noise
+        imgs_noisy = torch.sqrt(alpha_hat_t) * imgs_real + torch.sqrt(1 - alpha_hat_t) * noise
         noise_pred = self.network(imgs_noisy, ts)
 
         loss = self.loss(noise_pred, noise)
@@ -102,14 +117,14 @@ class DiffusionModule(LightningModule):
         # Begin reverse diffusion process.
         x = noise
 
-        alpha = torch.ones((num_imgs,), device=self.device) - self.beta
-        alpha = alpha[:, None, None, None]
-        alpha_hat = alpha.clone()
-
         with torch.no_grad():
             for t_step in reversed(range(self.t_max)):
                 # Create time step tensor injected using model's positional encoding.
                 ts = torch.ones((num_imgs,), dtype=torch.int64, device=self.device) * t_step
+
+                beta_t = self.beta[ts][:, None, None, None]
+                alpha_t = self.alpha[ts][:, None, None, None]
+                alpha_hat_t = self.alpha_hat[ts][:, None, None, None]
 
                 # Predict noise for this time step.
                 noise_pred = self.network(x, ts)
@@ -121,9 +136,7 @@ class DiffusionModule(LightningModule):
                     z = torch.normal(0, 1, shape, device=self.device)
 
                 # Compute denoised image for this time step.
-                x_sub = x - (1 - alpha) / torch.sqrt(1 - alpha_hat) * noise_pred
-                x = 1 / torch.sqrt(alpha) * x_sub + self.beta ** 2 * z
-
-                alpha_hat *= alpha
+                x_sub = x - (1 - alpha_t) / torch.sqrt(1 - alpha_hat_t) * noise_pred
+                x = 1 / torch.sqrt(alpha_t) * x_sub + beta_t ** 2 * z
 
         return x
